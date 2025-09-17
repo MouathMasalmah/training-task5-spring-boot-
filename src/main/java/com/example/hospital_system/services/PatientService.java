99c942cd7749fd6820824efb5908da2fb395c423
MouathMasalmah <masalmahmouath@gmail.com>
2025-09-17 17:13:25 +0300

    add Patient Service

---
+++ src/main/java/com/example/hospital_system/services/PatientService.java
@@ -0,0 +1,49 @@
+package com.example.hospital_system.services;
+
+import com.example.hospital_system.entities.Patient;
+import com.example.hospital_system.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Optional<Patient> getPatientById(int id) {
+        return patientRepository.findById(id);
+    }
+
+    public List<Patient> getPatientsByDoctor(int doctor_id) {
+        return patientRepository.findByDoctorId(doctor_id);
+    }
+
+    public Patient createPatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public Patient updatePatient(int id, Patient patientDetails) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patient not found"));
+
+        patient.setPatient_name(patientDetails.getPatient_name());
+        patient.setPhone_number(patientDetails.getPhone_number());
+        patient.setEmail(patientDetails.getEmail());
+        patient.setAddress(patientDetails.getAddress());
+        patient.setDate_of_birth(patientDetails.getDate_of_birth());
+        patient.setDoctor_id(patientDetails.getDoctor_id());
+
+        return patientRepository.save(patient);
+    }
+
+    public void deletePatient(int id) {
+        patientRepository.deleteById(id);
+    }
+}
