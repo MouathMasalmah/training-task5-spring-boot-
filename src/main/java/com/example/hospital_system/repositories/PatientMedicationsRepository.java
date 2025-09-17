a436afd24cff063ab297597952148a5c65ad38bf
MouathMasalmah <masalmahmouath@gmail.com>
2025-09-17 16:07:15 +0300

    add PatientMedications Repository

---
+++ src/main/java/com/example/hospital_system/repositories/PatientMedicationsRepository.java
@@ -0,0 +1,12 @@
+package com.example.hospital_system.repositories;
+
+import com.example.hospital_system.entities.PatientMedications;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PatientMedicationsRepository extends JpaRepository<PatientMedications, Integer> {
+    List<PatientMedications> findByPatientId(int patientId);
+    List<PatientMedications> findByMedicationId(int medicationId);
+}
